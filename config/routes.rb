1e347dba06575e280b6b55aa36d5ad1ede7a7b14
Brian Takita <brian.takita@gmail.com>
2008-04-07 00:24:39 -0700

    Adding rest of Restful Authentication generated files.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+
+  map.resource :session
+
   map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
   map.signup '/signup', :controller => 'users', :action => 'new'
   map.login '/login', :controller => 'sessions', :action => 'new'
