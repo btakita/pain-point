36eab41f9e6ce8cbe882051e83424fcca6a22447
Brian Takita <brian.takita@gmail.com>
2008-04-07 00:29:16 -0700

    Fixed loading all fixtures in spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,6 @@ Spec::Runner.configure do |config|
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
   config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
-  config.global_fixtures :all
+  config.global_fixtures = :all
   config.mock_with :rr
 end
