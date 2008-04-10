b17bcbde414f4c1b0e5fc9182887ca3b803f8506
Brian Takita <brian.takita@gmail.com>
2008-04-09 22:50:20 -0700

    Updating Erector fixed spec.

--- spec/controllers/pain_points_controller_spec.rb
+++ spec/controllers/pain_points_controller_spec.rb
@@ -3,10 +3,8 @@ require File.expand_path("#{File.dirname(__FILE__)}/../spec_helper")
 describe PainPointsController do
   describe "GET index" do
     it "renders a list of the PaintPoints" do
-      pending "fix when Erector is updated" do
-        get :index
-        response.body.should include(PainPoint.find(:first).name)
-      end
+      get :index
+      response.body.should include(PainPoint.find(:first).name)
     end
   end
 end
