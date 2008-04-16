265e208ac0e57732027f8ef36e8cd70d47f7ff7a
Brian Takita <brian.takita@gmail.com>
2008-04-16 00:17:11 -0700

    Going back to erb. Ugh.

--- spec/controllers/lobby_controller_spec.rb
+++ spec/controllers/lobby_controller_spec.rb
@@ -3,9 +3,26 @@ require File.expand_path("#{File.dirname(__FILE__)}/../spec_helper")
 describe LobbyController do
   describe "GET show" do
     it "renders the lobby" do
-      get 'show'
+      get :show
       response.should be_success
-      response.body.should include("Lobby")
+      response.body.should include("Welcome to Pain Point")
+    end
+
+    it "renders PainPoint list" do
+      pain_points = PainPoint.find(:all)
+      pain_points.should_not be_empty
+      get :show
+      pain_points.each do |pain_point|
+        unless response.body.include?(pain_point.name)
+          raise "response.body should include #{pain_point.name}"
+        end
+      end
+    end
+    
+    it "renders a create PainPoint form" do
+      get :show
+      doc = Hpricot(response.body)
+      doc.at("form[@action=#{pain_points_path}]").should_not be_nil
     end
   end
 
