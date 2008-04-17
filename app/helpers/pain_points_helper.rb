1388f1474b2b140b60245338d3cd2dcd63788170
Brian Takita <brian.takita@gmail.com>
2008-04-16 21:33:18 -0700

    Extracted pain_points/_list partial template.

---
+++ app/helpers/pain_points_helper.rb
@@ -0,0 +1,2 @@
+module PainPointsHelper
+end
--- spec/controllers/lobby_controller_spec.rb
+++ spec/controllers/lobby_controller_spec.rb
@@ -9,20 +9,10 @@ describe LobbyController do
     end
 
     it "renders PainPoint list" do
-      pain_points = PainPoint.find(:all)
-      pain_points.should_not be_empty
       get :show
-      pain_points.each do |pain_point|
-        unless response.body.include?(pain_point.name)
-          raise "response.body should include #{pain_point.name}"
-        end
-      end
-    end
-    
-    it "renders a create PainPoint form" do
-      get :show
-      doc = Hpricot(response.body)
-      doc.at("form[@action=#{pain_points_path}]").should_not be_nil
+      response.body.should include(
+        response.template.render(:partial => "pain_points/list")
+      )
     end
   end
 
