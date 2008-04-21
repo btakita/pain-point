1454b3b148e0bdfc2b4ebed8d9cf58880e924140
Brian Takita <brian.takita@gmail.com>
2008-04-21 00:00:50 -0700

    Testing invalid data being passed to #create.

--- spec/controllers/up_vote_submissions_controller_spec.rb
+++ spec/controllers/up_vote_submissions_controller_spec.rb
@@ -24,17 +24,45 @@ module VoteSubmissions
           login_as :quentin
         end
 
-        describe "and passed valid data" do
-          it "creates a Vote" do
-            post :create, :pain_point_id => pain_point.id
-            user.reload
-            user.votes.pain_points.should include(pain_point)
-          end
-
+        describe "and the User does not have a Vote for the PainPoint" do
           it "responds with nothing" do
             post :create, :pain_point_id => pain_point.id
             response.should be_success
             response.body.should be_blank
+          end
+          
+          describe "and passed valid data" do
+            it "creates a Vote" do
+              post :create, :pain_point_id => pain_point.id
+              user.reload
+              user.votes.pain_points.should include(pain_point)
+            end
+          end
+
+          describe "and passed invalid data" do
+            before do
+              vote = nil
+              stub.proxy(Vote).new do |new_vote|
+                vote = new_vote
+                stub.proxy(vote).valid? {false}
+                vote
+              end
+            end
+
+            # Notice there is duplication in the "does not create a Vote" and "responds with an error" Examples
+            it "does not create a Vote" do
+              lambda do
+                lambda do
+                  post :create, :pain_point_id => pain_point.id
+                end.should raise_error
+              end.should_not change {Vote.count}
+            end
+
+            it "responds with an error" do
+              lambda do
+                post :create, :pain_point_id => pain_point.id
+              end.should raise_error
+            end
           end
         end
       end
