702644b8bd8e52b6f50511b18998071b3b48b8c4
Brian Takita <brian.takita@gmail.com>
2008-04-21 15:26:02 -0700

    Rendering the selected up and down votes.

--- app/views/pain_points/pain_point.rb
+++ app/views/pain_points/pain_point.rb
@@ -1,11 +1,34 @@
 class Views::PainPoints::PainPoint < Erector::Widget
   def render
     li :id => "pain_point_#{pain_point.id}", :class => "pain_point" do
-      span "Up", :class => "up"
+      up_link
       text " "
-      span "Down", :class => "down"
+      down_link
       text " "
       link_to pain_point.name, edit_pain_point_path(pain_point)
     end
+  end
+
+  protected
+  def up_link
+    css_class = "up"
+    if @user
+      vote = user.votes.find_by_pain_point_id(pain_point.id)
+      if vote && vote.state == 'up'
+        css_class << " selected"
+      end
+    end
+    a "Up", :class => css_class
+  end
+
+  def down_link
+    css_class = "down"
+    if @user
+      vote = user.votes.find_by_pain_point_id(pain_point.id)
+      if vote && vote.state == 'down'
+        css_class << " selected"
+      end
+    end
+    a "Up", :class => css_class
   end
 end
--- spec/views/pain_points/pain_point_spec.rb
+++ spec/views/pain_points/pain_point_spec.rb
@@ -1,14 +1,86 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Views::PainPoints::PainPoint do
-  attr_reader :pain_point
+  attr_reader :pain_point, :user, :html, :doc
 
-  describe "when the current_user has voted the PainPoint up" do
+  describe "when there is no passed in User" do
+    before do
+      @pain_point = pain_points(:software_complexity)
+      @html = Views::PainPoints::PainPoint.new(
+        template,
+        :pain_point => pain_point
+      ).to_s
+      @doc = Hpricot(html)
+    end
+
+    it "does not render the up and down links with the selected class" do
+      doc.at("a.up.selected").should be_nil
+      doc.at("a.down.selected").should be_nil
+    end
+  end
+
+  describe "when the User has not voted on the PainPoint" do
+    before do
+      @pain_point = pain_points(:software_complexity)
+      @user = users(:quentin)
+      user.votes.pain_points.should_not include(pain_point)
+      
+      @html = Views::PainPoints::PainPoint.new(
+        template,
+        :user => user,
+        :pain_point => pain_point
+      ).to_s
+      @doc = Hpricot(html)
+    end
+
+    it "does not render the up and down links with the selected class" do
+      doc.at("a.up.selected").should be_nil
+      doc.at("a.down.selected").should be_nil
+    end
+  end
+
+  describe "when the User has voted on the PainPoint" do
     attr_reader :vote
     before(:each) do
       @vote = votes(:quentin_slow_tests)
       @pain_point = vote.pain_point
-      @current_user = vote.user
+      @user = vote.user
+    end
+
+    describe "when the User has voted the PainPoint up" do
+      before(:each) do
+        vote.up_vote
+        vote.state.should == 'up'
+        @html = Views::PainPoints::PainPoint.new(
+          template,
+          :user => user,
+          :pain_point => pain_point
+        ).to_s
+        @doc = Hpricot(html)
+      end
+
+      it "renders the up link with a selected class" do
+        doc.at("a.up.selected").should_not be_nil
+        doc.at("a.down.selected").should be_nil
+      end
+    end
+
+    describe "when the User has voted the PainPoint down" do
+      before(:each) do
+        vote.down_vote
+        vote.state.should == 'down'
+        @html = Views::PainPoints::PainPoint.new(
+          template,
+          :user => user,
+          :pain_point => pain_point
+        ).to_s
+        @doc = Hpricot(html)
+      end
+
+      it "renders the up link with a selected class" do
+        doc.at("a.up.selected").should be_nil
+        doc.at("a.down.selected").should_not be_nil
+      end
     end
   end
 end
