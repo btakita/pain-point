35d465b43dd560bf4c0627d3dbc780f8a36fc615
Brian Takita <brian.takita@gmail.com>
2008-04-21 16:11:38 -0700

    Refactored vote links.

--- app/views/pain_points/pain_point.rb
+++ app/views/pain_points/pain_point.rb
@@ -1,34 +1,23 @@
 class Views::PainPoints::PainPoint < Erector::Widget
   def render
     li :id => "pain_point_#{pain_point.id}", :class => "pain_point" do
-      up_link
+      vote_link 'up', 'Up'
       text " "
-      down_link
+      vote_link 'down', 'Down'
       text " "
       link_to pain_point.name, edit_pain_point_path(pain_point)
     end
   end
 
   protected
-  def up_link
-    css_class = "up"
+  def vote_link(state, text)
+    css_class = state
     if @user
       vote = user.votes.find_by_pain_point_id(pain_point.id)
-      if vote && vote.state == 'up'
+      if vote && vote.state == state
         css_class << " selected"
       end
     end
-    a "Up", :class => css_class
-  end
-
-  def down_link
-    css_class = "down"
-    if @user
-      vote = user.votes.find_by_pain_point_id(pain_point.id)
-      if vote && vote.state == 'down'
-        css_class << " selected"
-      end
-    end
-    a "Up", :class => css_class
+    a text, :class => css_class
   end
 end
