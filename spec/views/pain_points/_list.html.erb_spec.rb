544d81adfe028f98658e0d48655482700ff7917f
Brian Takita <brian.takita@gmail.com>
2008-04-21 15:31:20 -0700

    Using File.expand_path for requiring spec_helper.rb

--- spec/views/pain_points/_list.html.erb_spec.rb
+++ spec/views/pain_points/_list.html.erb_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../../spec_helper'
+require File.expand_path("#{File.dirname(__FILE__)}/../../spec_helper")
 
 describe "/pain_points/_list" do
   attr_reader :pain_points
