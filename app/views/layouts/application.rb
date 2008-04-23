35e34e8cb9741b7c6e6fcc6dd96ede9e9592ff62
Brian Takita <brian.takita@gmail.com>
2008-04-22 23:30:27 -0700

    Adding xmlbuilder.

--- app/views/layouts/application.rb
+++ app/views/layouts/application.rb
@@ -3,6 +3,12 @@ class Views::Layouts::Application < Erector::Widget
     html do
       head do
         title "Pain Point"
+        js_files = [
+          "jquery-1.2.3.js",
+          "xmlbuilder"
+        ]
+        javascript_include_tag(*js_files)
+
         stylesheet_link_tag 'typography', :cache => true
       end
 
