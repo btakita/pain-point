758a4ac0a5f6db08e47b7b6faab22890035142cc
Brian Takita <brian.takita@gmail.com>
2008-04-24 18:12:57 -0700

    Extracted common #refresh_links method on PainPointView.create.

--- public/javascripts/views/pain_point_view.js
+++ public/javascripts/views/pain_point_view.js
@@ -26,30 +26,34 @@
     this.a(direction, {'class': css_class, 'href': "#"});
   }
 
-  window.PainPointView = {
-    'create': function(pain_point) {
-      var view = $(new PainPointViewBuilder(pain_point).render());
-      view.find("a.up").click(function() {
-        pain_point.up_vote(function(data) {
-          if(data.vote_state == "up") {
-            view.find("a.up").addClass("selected");
-          } else {
-            view.find("a.up").removeClass("selected");
-          }
-          view.find("a.down").removeClass("selected");
-        });
-      });
-      view.find("a.down").click(function() {
-        pain_point.down_vote(function(data) {
-          view.find("a.up").removeClass("selected");
-          if(data.vote_state == "down") {
-            view.find("a.down").addClass("selected");
-          } else {
-            view.find("a.down").removeClass("selected");
-          }
-        });
-      });
-      return view;
+  window.PainPointView = new Object();
+  PainPointView.create = function(pain_point) {
+    var view = $(new PainPointViewBuilder(pain_point).render());
+    view.pain_point = pain_point;
+
+    view.refresh_links = function(data) {
+      if(this.pain_point.vote_state == "up") {
+        this.find("a.up").addClass("selected");
+        this.find("a.down").removeClass("selected");
+      } else if(this.pain_point.vote_state == "down") {
+        this.find("a.down").addClass("selected");
+        this.find("a.up").removeClass("selected");
+      } else {
+        this.find("a.up").removeClass("selected");
+        this.find("a.down").removeClass("selected");
+      }
     }
-  }
+
+    view.find("a.up").click(function() {
+      pain_point.up_vote(function(pain_point) {
+        view.refresh_links(pain_point);
+      });
+    });
+    view.find("a.down").click(function() {
+      pain_point.down_vote(function(data) {
+        view.refresh_links(pain_point);
+      });
+    });
+    return view;
+  };
 })(this);
