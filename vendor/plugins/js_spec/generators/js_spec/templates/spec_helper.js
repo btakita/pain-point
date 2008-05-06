141b466d963cfa0621cb50d7e0f94a70f1584885
Brian Takita <brian.takita@gmail.com>
2008-05-06 01:03:01 -0700

    Removing JSSpec.

--- vendor/plugins/js_spec/generators/js_spec/templates/spec_helper.js
+++
@@ -1,6 +0,0 @@
-// require('/implementations/your_js_file');
-// stylesheet('/stylesheets/your_css_file');
-
-Spec.reset = function() {
-  document.getElementById('js_spec_content').innerHTML = "";
-}
