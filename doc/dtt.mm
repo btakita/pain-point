a290db39427369018640e70a029a926cc69794f8
Brian Takita <brian.takita@gmail.com>
2008-05-10 17:30:22 -0700

    Adding DTT Presentation mind map.

---
+++ doc/dtt.mm
@@ -0,0 +1,21 @@
+<map version="0.7.1">
+<node TEXT="Developer Testing Tricks">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node TEXT="Introduction" POSITION="right">
+<node TEXT="Who uses rspec?"/>
+<node TEXT="Who uses Test::Unit?"/>
+<node TEXT="Who uses Shoulda?"/>
+<node TEXT="Most of my examples will be in Rspec"/>
+</node>
+<node TEXT="Installing rspec" POSITION="right"/>
+<node TEXT="First Example" POSITION="right">
+<node TEXT="Simple"/>
+<node TEXT="Explain the structure of a test">
+<node TEXT="Preconditions"/>
+<node TEXT="Implementation"/>
+<node TEXT="Postconditions"/>
+</node>
+</node>
+</node>
+</map>
