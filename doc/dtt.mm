609a11407871a516736459b2f66bae158228e137
Brian Takita <brian.takita@gmail.com>
2008-05-11 13:00:27 -0700

    Initial time estimates.

--- doc/dtt.mm
+++ doc/dtt.mm
@@ -2,22 +2,20 @@
 <node TEXT="Developer Testing Tricks">
 <edge WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="12"/>
-<node TEXT="Introduction" POSITION="right">
+<node TEXT="Introduction 5:00" POSITION="right">
 <node TEXT="Who uses rspec?"/>
 <node TEXT="Who uses Test::Unit?"/>
 <node TEXT="Who uses Shoulda?"/>
 <node TEXT="Most of my examples will be in Rspec"/>
 </node>
-<node TEXT="Test Types" POSITION="right">
+<node TEXT="Test Types 10:00" POSITION="right">
 <node TEXT="Method">
 <node TEXT="Automated"/>
 <node TEXT="Manual"/>
 </node>
 <node TEXT="Audience">
 <node TEXT="Developer"/>
-<node TEXT="Customer">
-<font NAME="DejaVu Sans Mono" SIZE="12"/>
-</node>
+<node TEXT="Customer"/>
 </node>
 <node TEXT="Scope">
 <node TEXT="Unit"/>
@@ -25,23 +23,8 @@
 <node TEXT="Integration"/>
 </node>
 </node>
-<node TEXT="Installing rspec" POSITION="right"/>
-<node TEXT="First Example" POSITION="right">
-<node TEXT="Simple"/>
-<node TEXT="Explain the structure of a test">
-<node TEXT="physical structure">
-<node TEXT="before"/>
-<node TEXT="execution"/>
-<node TEXT="after"/>
-</node>
-<node TEXT="logical structure">
-<node TEXT="preconditions"/>
-<node TEXT="implementation"/>
-<node TEXT="postconditions"/>
-</node>
-</node>
-</node>
-<node TEXT="Restful Authentication Example" POSITION="right">
+<node TEXT="Installing rspec 3:00" POSITION="right"/>
+<node TEXT="Restful Authentication Example 10:00" POSITION="right">
 <node TEXT="Install the Restful Authentication plugin"/>
 <node TEXT="Generate the files and tests"/>
 <node TEXT="Refactor the tests">
@@ -55,11 +38,42 @@
 </node>
 </node>
 </node>
-<node TEXT="Vote Example" POSITION="right">
-<node TEXT="Acts as State Machine">
+<node TEXT="Vote Example 10:00" POSITION="right">
+<node TEXT="Explain the structure of a test">
+<node TEXT="physical structure">
+<node TEXT="before"/>
+<node TEXT="execution"/>
+<node TEXT="after"/>
+</node>
+<node TEXT="logical structure">
+<node TEXT="preconditions"/>
+<node TEXT="implementation"/>
+<node TEXT="postconditions"/>
+</node>
+</node>
+<node TEXT="State Machine">
+<node TEXT="Introduce Acts as State Machine"/>
 <node TEXT="Test the Scenarios using nested describes"/>
 <node TEXT="Refactor to Fitnesse"/>
 </node>
+<node TEXT="Associations">
+<node TEXT=".up"/>
+<node TEXT=".down"/>
+</node>
+</node>
+<node TEXT="VoteSubmissionsController Examples 10:00" POSITION="right">
+<node TEXT="UpVoteSubmissionsController"/>
+<node TEXT="DownVoteSubmissionsController"/>
+<node TEXT="ExampleGroup methods that create Examples">
+<node TEXT=".should_require_login"/>
+<node TEXT="advantages"/>
+<node TEXT="disadvantages"/>
+</node>
+<node TEXT="Talk about duplication in the specs">
+<node TEXT="DRY"/>
+<node TEXT="Clarity (Straightforwardness)"/>
+</node>
+<node COLOR="#990000" TEXT="Refactor using Shared Describes"/>
 </node>
 </node>
 </map>
