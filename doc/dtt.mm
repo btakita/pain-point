5e4e1031328cb9cbf6300118c8adabf637b9071a
Brian Takita <brian.takita@gmail.com>
2008-05-11 12:28:17 -0700

    Added a couple of examples to mind map.

--- doc/dtt.mm
+++ doc/dtt.mm
@@ -8,13 +8,57 @@
 <node TEXT="Who uses Shoulda?"/>
 <node TEXT="Most of my examples will be in Rspec"/>
 </node>
+<node TEXT="Test Types" POSITION="right">
+<node TEXT="Method">
+<node TEXT="Automated"/>
+<node TEXT="Manual"/>
+</node>
+<node TEXT="Audience">
+<node TEXT="Developer"/>
+<node TEXT="Customer">
+<font NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+</node>
+<node TEXT="Scope">
+<node TEXT="Unit"/>
+<node TEXT="Functional"/>
+<node TEXT="Integration"/>
+</node>
+</node>
 <node TEXT="Installing rspec" POSITION="right"/>
 <node TEXT="First Example" POSITION="right">
 <node TEXT="Simple"/>
 <node TEXT="Explain the structure of a test">
-<node TEXT="Preconditions"/>
-<node TEXT="Implementation"/>
-<node TEXT="Postconditions"/>
+<node TEXT="physical structure">
+<node TEXT="before"/>
+<node TEXT="execution"/>
+<node TEXT="after"/>
+</node>
+<node TEXT="logical structure">
+<node TEXT="preconditions"/>
+<node TEXT="implementation"/>
+<node TEXT="postconditions"/>
+</node>
+</node>
+</node>
+<node TEXT="Restful Authentication Example" POSITION="right">
+<node TEXT="Install the Restful Authentication plugin"/>
+<node TEXT="Generate the files and tests"/>
+<node TEXT="Refactor the tests">
+<node TEXT="Nested describes">
+<node TEXT="Describe ExampleGroup Subclassing"/>
+<node TEXT="Scope">
+<node TEXT="Class"/>
+<node TEXT="Method"/>
+<node TEXT="Context"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Vote Example" POSITION="right">
+<node TEXT="Acts as State Machine">
+<node TEXT="Test the Scenarios using nested describes"/>
+<node TEXT="Refactor to Fitnesse"/>
 </node>
 </node>
 </node>
