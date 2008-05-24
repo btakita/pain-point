567ed91cc04ac181da712c4d3b00c8f056d9e4e3
Brian Takita <brian.takita@gmail.com>
2008-05-24 11:09:34 -0700

    Added spec for the logged in user.

--- spec/rails_integration/vote_submissions_spec.rb
+++ spec/rails_integration/vote_submissions_spec.rb
@@ -1,12 +1,14 @@
 require File.expand_path("#{File.dirname(__FILE__)}/../rails_integration_spec_helper")
 
 describe "A User on the home page" do
+  attr_reader :session_id
   before do
-    visits "/"
+    get "/"
+    @session_id = cookies["_pain-point_session"]
   end
 
-  describe "clicking the up vote" do
-    describe "when not logged in" do
+  describe "when not logged in" do
+    describe "clicking the up vote" do
       it "sends a message to redirect to /login" do
         pain_point = pain_points(:slow_tests)
         post(
@@ -15,10 +17,36 @@ describe "A User on the home page" do
           "X-Requested-With" => "XmlHttpRequest",
           "Accept" => "application/json, text/javascript, */*",
           "Content-Type" => "application/json; charset=UTF-8",
-          "Cookie" => "bc_test=true; _pain-point_session=BAh7CDoOcmV0dXJuX3RvMCIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6%250ARmxhc2g6OkZsYXNoSGFzaHsABjoKQHVzZWR7ADoMY3NyZl9pZCIlNWM2NmJm%250AOWM3Nzc0MzIxMDdmYzczOWJkNDNiODNhZmY%253D--dedbb152c476b5b0189f65e5491a17dce6190a08"
+          "Cookie" => "bc_test=true; _pain-point_session=#{session_id}"
         )
         status.should == 200
         JSON.parse(@response.body).should == {'type' => 'Redirect', 'attributes' => {'href' => new_session_path}}
+      end
+    end
+  end
+
+  describe "when logged in" do
+    attr_reader :user
+    before do
+      @user = users(:quentin)
+      visits login_path
+      post(session_path, {"login" => user.login, "password" => "test", "Cookie" => "bc_test=true; _pain-point_session=#{session_id}"})
+      @session_id = cookies["_pain-point_session"]
+    end
+
+    describe "clicking the up vote" do
+      it "sends a message to redirect to /login" do
+        pain_point = pain_points(:slow_tests)
+        post(
+          create_pain_point_up_vote_path(:pain_point_id => pain_point.to_param),
+          {"authenticity_token" => "somethingnew"}.to_json,
+          "X-Requested-With" => "XmlHttpRequest",
+          "Accept" => "application/json, text/javascript, */*",
+          "Content-Type" => "application/json; charset=UTF-8",
+          "Cookie" => "bc_test=true; _pain-point_session=#{session_id}"
+        )
+        status.should == 200
+        JSON.parse(@response.body).should == pain_point.user_data(user)
       end
     end
   end
