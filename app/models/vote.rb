9d01d66883994b85f71623d4fe5976fde5a05e06
Brian Takita <brian.takita@gmail.com>
2008-05-26 18:56:40 -0700

    Adding in commented out implementation of state machine.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,4 +5,23 @@ class Vote < ActiveRecord::Base
   validates_presence_of :user_id
   validates_presence_of :pain_point_id
   validates_uniqueness_of :user_id, :scope => [:pain_point_id]
+
+#  acts_as_state_machine :initial => :neutral
+#  state :neutral
+#  state :up
+#  state :down
+#
+#  event :up_vote do
+#    transitions :from => :neutral, :to => :up
+#    transitions :from => :down, :to => :up
+#    transitions :from => :up, :to => :neutral
+#  end
+#  alias_method :up_vote, :up_vote!
+#
+#  event :down_vote do
+#    transitions :from => :neutral, :to => :down
+#    transitions :from => :up, :to => :down
+#    transitions :from => :down, :to => :neutral
+#  end
+#  alias_method :down_vote, :down_vote!
 end
