de29c1ad402507fe2f8aa8a6c51740ea3f0ca669
Brian Takita <brian.takita@gmail.com>
2008-05-28 02:32:42 -0700

    Moving stuff around.

--- doc/dtt.mm
+++ doc/dtt.mm
@@ -158,14 +158,21 @@
 <node TEXT="Introduce Erector"/>
 <node TEXT="Erector view spec"/>
 </node>
-<node TEXT="Custom Matcher" POSITION="right">
-<node TEXT="have_link.rb in rspec_hpricot_matchers plugin"/>
-<node TEXT="using fork on github to introduce features to existing projects"/>
-</node>
 <node TEXT="Audience participation 30:00" POSITION="right">
 <node TEXT="Introduce Git"/>
 <node TEXT="Instruct audience to jump to 04-vote-submissions-controller branch"/>
 <node TEXT="Instruct audience to create DownVoteSubmissionsController"/>
+</node>
+<node TEXT="Custom Matcher" POSITION="right">
+<node TEXT="have_link.rb in rspec_hpricot_matchers plugin"/>
+<node TEXT="using fork on github to introduce features to existing projects"/>
+</node>
+<node TEXT="Test Double Example" POSITION="right">
+<node TEXT="Introduce RR"/>
+<node TEXT="Mocks"/>
+<node TEXT="Stubs"/>
+<node TEXT="Mock Proxy"/>
+<node TEXT="Stub Proxy"/>
 </node>
 <node TEXT="Extracting up and down links into JS example 20:00" POSITION="right">
 <node TEXT="Client/Server architecture">
@@ -176,13 +183,6 @@
 <node TEXT="screw unit + server"/>
 </node>
 <node TEXT="Selenium Testing 10:00" POSITION="right"/>
-<node TEXT="Test Double Example" POSITION="right">
-<node TEXT="Introduce RR"/>
-<node TEXT="Mocks"/>
-<node TEXT="Stubs"/>
-<node TEXT="Mock Proxy"/>
-<node TEXT="Stub Proxy"/>
-</node>
 <node TEXT="Audience participation 30:00" POSITION="right"/>
 <node TEXT="send instructions to participants" POSITION="left">
 <node TEXT="notify that this is a preliminary outline, it will change the following week"/>
